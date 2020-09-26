3ca1ca4e2a6078419835af8a1d7a10e92ac5c44e
Ashley Madera <ashleymadera@Ashleys-MBP.home>
2020-09-25 20:27:37 -0400

    bugs fixed

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let calculateBtn = document.querySelector(".calculateBtn");
 let amountOwed = document.getElementById("amountOwed");
 let payBtn = document.querySelector(".payBtn");
+let imageContainer = document.querySelector('.imageContainer')
+let container = document.querySelector('.container')
 //Global currentCatTax variable
 let currentCatTax = 0;
+
+
 
 // TODO: calcButtonClick function
 // Function should handle the following items:
@@ -18,6 +22,7 @@ function calcButtonClick() {
   currentCatTax = Math.floor(Math.random() * Math.floor(20));
   if (currentCatTax != 0) {
     amountOwed.innerText = `You owe ${currentCatTax} cat tax! Pay up!`;
+    payBtn.removeAttribute('hidden')
     payBtn.innerText = "Pay Cat Tax";
   } else {
     amountOwed.innerText = `You owe ${currentCatTax} cat tax! You've escaped this time!`;
@@ -32,17 +37,33 @@ function calcButtonClick() {
 // 4) If the cat tax was payable (amount was greater than 0) when the button was clicked, make a call to the cat api to get a cat image (https://api.thecatapi.com/v1/images/search)
 // 5) Once the cat image is retrieved, append the image to the image container
 // 6) If the cat wax was not payable (amount was less than or equal to 0) when the button was clicked, replace the entire contents of the container with the gif found here (https://gfycat.com/snivelingbeautifuljoey-cat)
+async function catImg() {
+  const response = await axios.get('https://api.thecatapi.com/v1/images/search')
+  const data = response.data[0].url
+  const img = document.createElement("img");
+  img.src = data
+  imageContainer.appendChild(img)
+}
 
 function payButton() {
-  if (currentCatTax > 0) {
-    currentCatTax -= 1;
-    if (currentCatTax <= 0) {
-      amountOwed.innerText = "Your debts are paid...";
-    } else {
+  currentCatTax -= 1;
+  if (currentCatTax > -1) {
+
+    if (currentCatTax > 0) {
+      catImg()
       amountOwed.innerText = `You still owe ${currentCatTax} cat tax! Pay up!`;
+      console.log(currentCatTax);
+    } else {
+      catImg()
+      amountOwed.innerText = "Your debts are paid...";
     }
+  } else {
+    container.innerHTML = "<iframe src='https://gfycat.com/ifr/SnivelingBeautifulJoey' frameborder='0' scrolling='no' allowfullscreen width='640' height='753'></iframe>"
+
   }
+
 }
+
 
 //refernce : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
